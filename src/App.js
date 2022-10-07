feac187644a6b3db0394d26e4ebeb725f1ff2b50
callmemg <oremgitonga22@gmail.com>
2022-10-07 23:18:16 +0300

    routing, tailwind configuration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// layout components - header & footer
+import { Header, Footer } from "./components/index";
+
+// pages
+import { Home, PropertyDetails } from "./pages/index";
 
 const App = () => {
-  return <div>react app</div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/property/:id" element={<PropertyDetails />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { default as Banner } from "./Banner";
+export { default as CountryDropdown } from "./CountryDropdown";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
+export { default as House } from "./House";
+export { default as HouseContext } from "./HouseContext";
+export { default as HouseList } from "./HouseList";
+export { default as PriceRangeDropdown } from "./PriceRangeDropdown";
+export { default as PropertyDropdown } from "./PropertyDropdown";
+export { default as Search } from "./Search";
